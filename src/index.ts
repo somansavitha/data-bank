7c12ac32be62bb65bb50e6cf461c5dc001e58605
somansavitha <savisoman92@gmail.com>
2025-10-22 17:23:58 +0530

    process  env added

--- src/index.ts
+++ src/index.ts
@@ -19,5 +19,5 @@ app.use("/api/website-details", websiteDetailsRoutes);
 app.use("/api/service-details", serviceDetailsRoutes);
 app.use("/api/product-details", productDetailsRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
